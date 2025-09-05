7c37a2d90bc205e00e2fae3ac32fda8a01ec23d7
Natalie Danner <danner15@usf.edu>
2025-09-04 22:04:26 -0400

    Created Variables

--- cc_2a.js
+++ cc_2a.js
@@ -1,3 +1,10 @@
 // Coding Challenge 00
 
-// Code goes here
+let productname = "waterBottle";
+console.log(productname);
+
+let costPerUnit = 10;
+let basePrice = 15;
+let discountRate = 0.2;
+let salesTaxRate = 0.07;
+let fixedMonthlyCosts = 500;
