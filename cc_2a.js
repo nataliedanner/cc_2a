6472d31aaa8138874392ef0b1d2cb42bbd8d85a5
Natalie Danner <danner15@usf.edu>
2025-09-06 11:56:51 -0400

    Added More Pricing & Profit Metrics

--- cc_2a.js
+++ cc_2a.js
@@ -7,9 +7,15 @@ let discountRate = 0.2;
 let salesTaxRate = 0.07;
 let fixedMonthlyCosts = 500;
 
-let discountedPrice = (basePrice * (1 - discountRate))
-console.log(discountedPrice);
+let discountedPrice = (basePrice * (1 - discountRate));
+console.log(discountedPrice.);
 
 let finalPriceWithTax = (discountedPrice * (1 + salesTaxRate));
 console.log(finalPriceWithTax);
+
+let profitPerUnit = (finalPriceWithTax - costPerUnit);
+console.log(profitPerUnit);
+
+let breakEvenUnits = (Math.ceil(fixedMonthlyCosts / profitPerUnit));
+console.log(breakEvenUnits);
 
